74d8eaf8905abd098f3ca4bedabeaf5b6709d414
= <=>
2016-05-09 22:10:00 +0800

    finish ch01

---
+++ ch01/ch01.h
@@ -0,0 +1,81 @@
+#ifndef _CH01_H 
+#define _CH01_H
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <strings.h>
+#include <string.h>
+#include <errno.h>
+#include <stdarg.h>
+#include <syslog.h>
+
+#define MAXLINE 1024 
+
+int daemon_proc;              //若该数为0，则将出错信息直接打印到屏幕，若该数为1，则调用syslog()函数，由其决定.
+
+void err_quit(const char *fmt,...);
+void err_sys(const char* fmt,...);
+static void err_doit(int,int,const char*,va_list);
+
+void err_quit(const char *fmt,...)
+{
+	va_list ap;
+	
+	va_start(ap,fmt);   //将第一个可变形参的参数地址传递给ap
+	err_doit(0,LOG_ERR,fmt,ap);
+	va_end(ap);
+
+	exit(1);
+}
+
+
+void err_sys(const char *fmt,...) 
+{
+	va_list ap;
+	
+	va_start(ap,fmt);
+	err_doit(1,LOG_ERR,fmt,ap);
+	va_end(ap);
+
+	exit(1);
+}
+
+static void err_doit(int errnoflag,int level,const char *fmt,va_list ap)
+{
+	int errno_save,n;
+	char buf[MAXLINE + 1]; 
+
+	errno_save = errno;   //打印当前出错的系统提示
+
+
+#ifdef HAVE_VSNPRINF           //防止旧编译器没有该函数
+	vsnprintf(buf,MAXLINE,fmt,ap);
+#else
+	vsprintf(buf,fmt,ap);
+#endif
+
+	n = strlen(buf);
+	if(errnoflag)     //errnoflag为1，则打印系统提示的出错信息
+		snprintf(buf + n, MAXLINE - n, ": %s",strerror(errno_save));
+	strcat(buf,"\n");
+	
+	if(daemon_proc)
+	{
+		syslog(level,buf);
+	}
+	else
+	{
+		fflush(stdout);
+		fputs(buf,stderr);
+		fflush(stderr);
+	}
+	return;
+}
+
+
+#endif
---
+++ ch01/daytimetcpsrv.c
@@ -0,0 +1,53 @@
+#include "ch01.h"
+#include <time.h>
+
+
+
+/*
+	TCP服务器的创建流程：
+	socket(创建socket套接字描述符) -> bind(将套接字与服务器地址和端口相绑定)
+	-> listen(监听套接字) -> accept(与客户建立连接) -> read(与客户进行通信)
+*/
+
+int main(int argc,char **argv)
+{
+	int listenfd, connfd;
+	struct sockaddr_in servaddr;
+	char buff[MAXLINE];
+	time_t ticks;
+
+	if((listenfd = socket(AF_INET,SOCK_STREAM,0)) < 0)
+	{
+		err_sys("socket error");
+	}
+
+	bzero(&servaddr,sizeof(servaddr));
+	servaddr.sin_family = AF_INET;
+	servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+	servaddr.sin_port = htons(13);
+
+	
+	if(bind(listenfd,(struct sockaddr*)&servaddr,sizeof(servaddr)) < 0)
+		err_sys("bind error");
+
+	if(listen(listenfd,5) < 0)
+		err_sys("listen error");
+
+	for( ; ; )
+	{
+		if((connfd = accept(listenfd, (struct sockaddr*)NULL,NULL)) < 0) 
+			err_sys("accept error");
+
+		ticks = time(NULL);    //获得1970年一月一日到当前时间的秒数
+		snprintf(buff,sizeof(buff),"%.24s\r\n",ctime(&ticks));   //转换成当前时区的具体时间
+		
+		if(write(connfd,buff,strlen(buff)) < 0)
+			err_sys("write error");
+
+		if(close(connfd) < 0)
+			err_sys("close error");
+
+	}
+	
+	exit(0);
+}
