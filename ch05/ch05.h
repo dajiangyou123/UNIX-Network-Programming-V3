0b096c15058088e1a9f6b6c73fb6891add1ddf33
dajiangyou123 <fyly123@gmail.com>
2016-05-15 21:00:27 +0800

    finish TCP echo

--- ch05/ch05.h
+++ ch05/ch05.h
@@ -224,30 +224,49 @@ char *Fgets(char *buf, int n, FILE *fp)
 }
 
 
-//从一个描述符读取n字节或者一行
-ssize_t Readline(int fd, void *vptr, size_t n)
+//从一个描述符读取n字节或者一行,每次读取一个字节，方便查看该字节是否是换行符'\n'
+ssize_t readline(int fd, void *vptr, size_t maxlen)
 {
-	size_t nleft;
-	ssize_t nread;
-	char *ptr;
+	ssize_t n,rc;
+	char c,*ptr;
 
 	ptr = vptr;
-	nleft = n;
-	while (nleft > 0)
+
+	for(n = 1;n < maxlen;n++)
 	{
-		if((nread = read(fd,ptr,nleft)) < 0)
+	again:
+		if(( rc = read(fd,&c,1)) == 1)
+		{
+			*ptr++ = c; 
+			if(c == '\n')
+				break;
+		}
+		else if(rc == 0)
+		{
+			*ptr = 0;                  //遇到EOF，结束read
+			return n - 1;              //因为最后读取到的是EOF，不计入字节数。同时'\0'也不计入。
+		}
+		else
 		{
 			if(errno == EINTR)
-				nread = 0;            //当前被系统信号中断，再次调用read()
-			else
-				err_sys("read error");
+				goto again;
+			return -1;
 		}
-		else if(nread == 0)
-			break;          //EOF
-		nleft -= nread;
-		ptr += nread;
 	}
-	return n - nleft;             //return >= 0
+
+	*ptr = 0;
+	return n;
+}
+
+
+//readline的包裹函数
+ssize_t Readline(int fd, void *vptr, size_t maxlen)
+{
+	ssize_t n;
+	if((n = readline(fd,vptr,maxlen)) == -1)
+		err_sys("readline error");
+
+	return n;
 }
 
 //fputs的包裹函数
