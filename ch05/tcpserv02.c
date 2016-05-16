610598ff4eb16c16f4cce97ba6c005f4aa2f79bd
dajiangyou123 <fyly123@gmail.com>
2016-05-16 21:17:24 +0800

    finish 5.9

---
+++ ch05/tcpserv02.c
@@ -0,0 +1,60 @@
+//TCP回射服务器程序，多进程，加入信号处理机制，处理僵尸进程
+
+#include "ch05.h"
+#include <signal.h>
+#include <sys/wait.h>
+
+
+void sig_child(int signo)
+{
+	pid_t pid;
+	int stat;
+
+	pid = wait(&stat);
+	printf("child %d terminated\n", pid);
+	return;
+}
+
+
+int main(int argc, char **argv)
+{
+	int listenfd, connfd;
+	pid_t childpid;
+	socklen_t clilen;
+	struct sockaddr_in cliaddr,servaddr;
+
+	bzero(&servaddr,sizeof(servaddr));
+	//客户的地址不需要事先全置为0
+
+	listenfd = Socket(AF_INET,SOCK_STREAM,0);
+	
+
+	servaddr.sin_family = AF_INET; 
+	servaddr.sin_port = htons(SERV_PORT);
+	servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+	
+	Bind(listenfd,(struct sockaddr *)&servaddr,sizeof(servaddr));
+
+	Listen(listenfd,LISTENQ);
+
+	//设置信号处理函数
+	signal(SIGCHLD, sig_child);
+
+	for( ; ; )
+	{
+		clilen = sizeof(cliaddr);
+		connfd = Accept(listenfd,(struct sockaddr *)&cliaddr,&clilen);
+
+		if((childpid = Fork()) == 0)    //子进程
+		{
+			Close(listenfd);           //关闭监听套接字 
+			str_echo(connfd);          //处理客户的请求
+			exit(0);
+		}
+
+		Close(connfd);             //父进程关闭已连接描述符
+
+	}
+
+	return 0;
+}
